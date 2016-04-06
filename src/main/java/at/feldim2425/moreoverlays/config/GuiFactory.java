014596da47f9989592321ff943260e9a1bac85df
feldim2425 <feldim2425@gmail.com>
2016-04-06 17:39:23 +0200

    Added Config for Light Overlay range

---
+++ src/main/java/at/feldim2425/moreoverlays/config/GuiFactory.java
@@ -0,0 +1,29 @@
+package at.feldim2425.moreoverlays.config;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+import java.util.Set;
+
+public class GuiFactory implements IModGuiFactory {
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+
+    }
+
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return ModConfigGui.class;
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+        return null;
+    }
+}
