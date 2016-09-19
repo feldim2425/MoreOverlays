bc673e0ad8776825675542efa6b6489c0f8594aa
mezz <tehgeek@gmail.com>
2016-09-19 16:05:57 -0700

    Improve startup time by using new JEI API to get filtered items

--- src/main/java/at/feldim2425/moreoverlays/itemsearch/JeiModule.java
+++ src/main/java/at/feldim2425/moreoverlays/itemsearch/JeiModule.java
@@ -1,22 +1,15 @@
 package at.feldim2425.moreoverlays.itemsearch;
 
-import mezz.jei.ItemFilter;
 import mezz.jei.api.*;
 import mezz.jei.input.IKeyable;
 
 import javax.annotation.Nonnull;
 
 @JEIPlugin
-public class JeiModule implements IModPlugin {
+public class JeiModule extends BlankModPlugin {
 
-    public static ItemFilter filter;
     public static IItemListOverlay overlay;
     public static IKeyable keyableOverlay;
-
-    @Override
-    public void register(@Nonnull IModRegistry registry) {
-        filter = new ItemFilter(registry.getItemRegistry());
-    }
 
     @Override
     public void onRuntimeAvailable(@Nonnull IJeiRuntime jeiRuntime) {
