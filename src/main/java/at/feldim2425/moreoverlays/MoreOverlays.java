fe3dc95a880874f30716ec7e3b2800b8a7c26655
Unknown <feldim2425@gmail.com>
2018-07-23 18:06:23 +0200

    Updated Version  + Dependencies

--- src/main/java/at/feldim2425/moreoverlays/MoreOverlays.java
+++ src/main/java/at/feldim2425/moreoverlays/MoreOverlays.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = MoreOverlays.MOD_ID, updateJSON = MoreOverlays.UPDATE_JSON, version = MoreOverlays.VERSION, name = MoreOverlays.NAME, clientSideOnly = true, dependencies = "required-after:forge@[14.22.0.2452,);after:jei@[4.7.11.102,);", guiFactory = "at.feldim2425.moreoverlays.config.GuiFactory")
+@Mod(modid = MoreOverlays.MOD_ID, updateJSON = MoreOverlays.UPDATE_JSON, version = MoreOverlays.VERSION, name = MoreOverlays.NAME, clientSideOnly = true, dependencies = "required-after:forge@[14.23.4.2739,);after:jei@[4.11.0.202,);", guiFactory = "at.feldim2425.moreoverlays.config.GuiFactory")
 public class MoreOverlays {
 
 	public static final String MOD_ID = "moreoverlays";
 	public static final String NAME = "MoreOverlays";
-	public static final String VERSION = "1.13";
+	public static final String VERSION = "1.14";
 	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/feldim2425/Mod_Update-JSONs/master/MoreOverlays.json";
 
 
