44dc4958c181c982d83b62db621ae5ebef51685b
Chris Tilden <chris.tilden@gmail.com>
2019-07-03 21:52:08 -0700

    adds boilerplate code for managing mod configuration and events

---
+++ src/main/java/at/feldim2425/moreoverlays/events/EventBusHelper.java
@@ -0,0 +1,44 @@
+package at.feldim2425.moreoverlays.events;
+
+import java.util.function.Consumer;
+
+import net.minecraftforge.fml.event.lifecycle.ModLifecycleEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class EventBusHelper {
+	private static IEventBus getInstance() {
+		return MinecraftForge.EVENT_BUS;
+	}
+
+	public static <T extends Event> void addListener(Class<T> eventType, Consumer<T> listener) {
+		IEventBus eventBus = getInstance();
+		eventBus.addListener(EventPriority.NORMAL, false, eventType, listener);
+	}
+
+	public static <T extends Event> void removeListener(Consumer<T> listener) {
+		IEventBus eventBus = getInstance();
+		eventBus.unregister(listener);
+	}
+
+	public static <T extends ModLifecycleEvent> void addLifecycleListener(IEventBus eventBus, Class<T> eventType, Consumer<T> listener) {
+		eventBus.addListener(EventPriority.NORMAL, false, eventType, listener);
+	}
+
+	public static void register(Object object) {
+		IEventBus eventBus = getInstance();
+		eventBus.register(object);
+	}
+
+	public static void unregister(Object object) {
+		IEventBus eventBus = getInstance();
+		eventBus.unregister(object);
+	}
+
+	public static void post(Event event) {
+		IEventBus eventBus = getInstance();
+		eventBus.post(event);
+	}
+}
